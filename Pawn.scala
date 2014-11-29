8f4931f8b0573b395b354f97ff01cd39f50b4d0c
Michael Figueiredo <MFigueiredo309@gmail.com>
2014-11-28 22:34:08 -0500

    Fixed Typo in Pawn Board Accesses

--- Pawn.scala
+++ Pawn.scala
@@ -22,24 +22,24 @@ class Pawn(r: Int, f: File, color: String) extends Piece {
        if(isInStartPosition) {
 
          // White Pawn Up 2
-         if(Board.pieceAt(r + 2, f).isBlank == true) {
+         if(b.pieceAt(r + 2, f).isBlank == true) {
            moveList = moveList ++ {(r + 2, f)}
          }
 
        }
 
       //White Pawn Up 1
-      if(Board.pieceAt(r + 1, f).isBlank == true) {
+      if(b.pieceAt(r + 1, f).isBlank == true) {
         moveList = moveList ++ {(r + 1, f)}
       }
 
       // White Pawn Attack Board Left
-      if ((Board.pieceAt(r + 1, f - 1).isWhite == false) && (Board.pieceAt(r + 1, f - 1).isBlank == false)) {
+      if ((b.pieceAt(r + 1, f - 1).isWhite == false) && (b.pieceAt(r + 1, f - 1).isBlank == false)) {
         moveList = moveList ++ {(r + 1, f - 1)}
       }
 
       // White Pawn Attack Board Right
-      if ((Board.pieceAt(r + 1, f + 1).isWhite == false) && (Board.pieceAt(r + 1, f + 1).isBlank == false)) {
+      if ((b.pieceAt(r + 1, f + 1).isWhite == false) && (b.pieceAt(r + 1, f + 1).isBlank == false)) {
         moveList = moveList ++ {(r + 1, f + 1)}
       }
 
@@ -50,24 +50,24 @@ class Pawn(r: Int, f: File, color: String) extends Piece {
       if(isInStartPosition) {
 
         // Black Pawn Down 2
-        if(Board.pieceAt(r - 2, f).isBlank == true) {
+        if(b.pieceAt(r - 2, f).isBlank == true) {
           moveList = moveList ++ {(r - 2, f)}
         }
 
       }
 
       // Black Pawn Down 1
-      if(Board.pieceAt(r - 1, f).isBlank == true) {
+      if(b.pieceAt(r - 1, f).isBlank == true) {
         moveList = moveList ++ {(r - 1, f)}
       }
 
       // Black Pawn Attack Board Left
-      if ((Board.pieceAt(r - 1, f - 1).isWhite == false) && (Board.pieceAt(r - 1, f - 1).isBlank == false)) {
+      if ((b.pieceAt(r - 1, f - 1).isWhite == false) && (b.pieceAt(r - 1, f - 1).isBlank == false)) {
         moveList = moveList ++ {(r - 1, f - 1)}
       }
 
       //Black Pawn Attack Board Right
-      if ((Board.pieceAt(r - 1, f + 1).isWhite == false) && (Board.pieceAt(r - 1, f + 1).isBlank == false)) {
+      if ((b.pieceAt(r - 1, f + 1).isWhite == false) && (b.pieceAt(r - 1, f + 1).isBlank == false)) {
         moveList = moveList ++ {(r - 1, f + 1)}
       }
 
