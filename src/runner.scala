e9a3f03502f06ca14d98ae4769069cf4393de175
John Paul Welsh <wearing2raincoats@yahoo.com>
2014-12-11 20:27:35 -0500

    Comments for what we'll do in runner.scala

--- src/runner.scala
+++ src/runner.scala
@@ -7,4 +7,25 @@ import javax.json.JsonStructure
 
 object Main extends App {
 
+  val DEPTH_LIMIT = 2
+
+  def pollForJSON() = "0"
+
+  def main(args: Array[String]) = {
+    //Do pollForJSON every 5 seconds
+    //Parse it into pieces
+    //if the "ready" property is true {
+    //  Get "lastMoveNumber"
+    //  if "lastMoveNumber" matches the one we're keeping track of {
+    //    val oppMove = "lastMove"
+    //    val currBoard = make a Board out of the current Board, with "lastMove" applied
+    //    val ourMove = Common.alphaBetaSearch(currBoard, DEPTH_LIMIT)
+    //    send ourMove to server
+    //  } else {
+    //    We dinked it up
+    //  }
+    //} else {
+    //  Do nothing
+    //}
+  }
 }
