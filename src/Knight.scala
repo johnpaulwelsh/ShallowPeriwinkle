c1ceb9f163bba0e49222fd617265717e7a57acdb
John Paul Welsh <wearing2raincoats@yahoo.com>
2014-12-12 22:04:49 -0500

    Adding Mike's Knight code. We ignore master forever

--- src/Knight.scala
+++ src/Knight.scala
@@ -5,7 +5,149 @@ class Knight(r: Int, f: Int, isW: Boolean) extends Piece {
   var file = f
   var isInStartPosition = true
 
-  override def availableMoves(b: Board) = {
-    List[String]()
+  override def availableMoves(b: Board): List[String] = {
+
+    var moveList = List[String]()
+
+    if (isWhite) {
+
+      if((rank + 2 > 0) && (rank + 2 < 9) && (file - 1 > 0) && (file - 1 < 9)) {
+
+        var possibleSpot = b.pieceAt(rank+2, file-1)
+        if (possibleSpot.isWhite == false) {
+          moveList = moveList ::: List("N" + rank + file + possibleSpot.rank + possibleSpot.file)
+        }
+
+      }
+      if((rank + 2 > 0) && (rank + 2 < 9) && (file + 1 > 0) && (file + 1 < 9)) {
+
+        var possibleSpot = b.pieceAt(rank+2, file+1)
+        if (possibleSpot.isWhite == false) {
+          moveList = moveList ::: List("N" + rank + file + possibleSpot.rank + possibleSpot.file)
+        }
+
+      }
+      if((rank + 1 > 0) && (rank + 1 < 9) && (file + 2 > 0) && (file + 2 < 9)) {
+
+        var possibleSpot = b.pieceAt(rank+1, file+2)
+        if (possibleSpot.isWhite == false) {
+          moveList = moveList ::: List("N" + rank + file + possibleSpot.rank + possibleSpot.file)
+        }
+
+      }
+      if((rank - 1 > 0) && (rank - 1 < 9) && (file + 2 > 0) && (file + 2 < 9)) {
+
+        var possibleSpot = b.pieceAt(rank-1, file+2)
+        if (possibleSpot.isWhite == false) {
+          moveList = moveList ::: List("N" + rank + file + possibleSpot.rank + possibleSpot.file)
+        }
+
+      }
+      if((rank - 2 > 0) && (rank - 2 < 9) && (file + 1 > 0) && (file + 1 < 9)) {
+
+        var possibleSpot = b.pieceAt(rank-2, file+1)
+        if (possibleSpot.isWhite == false) {
+          moveList = moveList ::: List("N" + rank + file + possibleSpot.rank + possibleSpot.file)
+        }
+
+      }
+      if((rank - 2 > 0) && (rank - 2 < 9) && (file - 1 > 0) && (file - 1 < 9)) {
+
+        var possibleSpot = b.pieceAt(rank-2, file-1)
+        if (possibleSpot.isWhite == false) {
+          moveList = moveList ::: List("N" + rank + file + possibleSpot.rank + possibleSpot.file)
+        }
+
+      }
+      if((rank - 1 > 0) && (rank - 1 < 9) && (file - 2 > 0) && (file - 2 < 9)) {
+
+        var possibleSpot = b.pieceAt(rank-1, file-2)
+        if (possibleSpot.isWhite == false) {
+          moveList = moveList ::: List("N" + rank + file + possibleSpot.rank + possibleSpot.file)
+        }
+
+      }
+      if((rank + 1 > 0) && (rank + 1 < 9) && (file - 2 > 0) && (file - 2 < 9)) {
+
+        var possibleSpot = b.pieceAt(rank+1, file-2)
+        if (possibleSpot.isWhite == false) {
+          moveList = moveList ::: List("N" + rank + file + possibleSpot.rank + possibleSpot.file)
+        }
+
+      }
+
+
+    }
+
+    else {
+
+      if((rank + 2 > 0) && (rank + 2 < 9) && (file - 1 > 0) && (file - 1 < 9)) {
+
+        var possibleSpot = b.pieceAt(rank+2, file-1)
+        if ((possibleSpot.isWhite == true) || (possibleSpot.isBlank)) {
+          moveList = moveList ::: List("N" + rank + file + possibleSpot.rank + possibleSpot.file)
+        }
+
+      }
+      if((rank + 2 > 0) && (rank + 2 < 9) && (file + 1 > 0) && (file + 1 < 9)) {
+
+        var possibleSpot = b.pieceAt(rank+2, file+1)
+        if ((possibleSpot.isWhite == true) || (possibleSpot.isBlank)) {
+          moveList = moveList ::: List("N" + rank + file + possibleSpot.rank + possibleSpot.file)
+        }
+
+      }
+      if((rank + 1 > 0) && (rank + 1 < 9) && (file + 2 > 0) && (file + 2 < 9)) {
+
+        var possibleSpot = b.pieceAt(rank+1, file+2)
+        if ((possibleSpot.isWhite == true) || (possibleSpot.isBlank)) {
+          moveList = moveList ::: List("N" + rank + file + possibleSpot.rank + possibleSpot.file)
+        }
+
+      }
+      if((rank - 1 > 0) && (rank - 1 < 9) && (file + 2 > 0) && (file + 2 < 9)) {
+
+        var possibleSpot = b.pieceAt(rank-1, file+2)
+        if ((possibleSpot.isWhite == true) || (possibleSpot.isBlank)) {
+          moveList = moveList ::: List("N" + rank + file + possibleSpot.rank + possibleSpot.file)
+        }
+
+      }
+      if((rank - 2 > 0) && (rank - 2 < 9) && (file + 1 > 0) && (file + 1 < 9)) {
+
+        var possibleSpot = b.pieceAt(rank-2, file+1)
+        if ((possibleSpot.isWhite == true) || (possibleSpot.isBlank)) {
+          moveList = moveList ::: List("N" + rank + file + possibleSpot.rank + possibleSpot.file)
+        }
+
+      }
+      if((rank - 2 > 0) && (rank - 2 < 9) && (file - 1 > 0) && (file - 1 < 9)) {
+
+        var possibleSpot = b.pieceAt(rank-2, file-1)
+        if ((possibleSpot.isWhite == true) || (possibleSpot.isBlank)) {
+          moveList = moveList ::: List("N" + rank + file + possibleSpot.rank + possibleSpot.file)
+        }
+
+      }
+      if((rank - 1 > 0) && (rank - 1 < 9) && (file - 2 > 0) && (file - 2 < 9)) {
+
+        var possibleSpot = b.pieceAt(rank-1, file-2)
+        if ((possibleSpot.isWhite == true) || (possibleSpot.isBlank)) {
+          moveList = moveList ::: List("N" + rank + file + possibleSpot.rank + possibleSpot.file)
+        }
+
+      }
+      if((rank + 1 > 0) && (rank + 1 < 9) && (file - 2 > 0) && (file - 2 < 9)) {
+
+        var possibleSpot = b.pieceAt(rank+1, file-2)
+        if ((possibleSpot.isWhite == true) || (possibleSpot.isBlank)) {
+          moveList = moveList ::: List("N" + rank + file + possibleSpot.rank + possibleSpot.file)
+        }
+
+      }
+
+    }
+
+    return moveList
   }
 }
