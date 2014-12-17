bf0e0366f50237d2c8e665de08adc1a089f21bda
John Paul Welsh <wearing2raincoats@yahoo.com>
2014-12-17 13:04:11 -0500

    Adjusted compile script

--- compile.sh
+++ compile.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 #Courtesy of Alvin Alexander at alvinalexander.com
+# Remove all .class files from this directory before we compile
+rm *.class
+
 THE_CLASSPATH=.
 for i in `ls *.jar`
 do
   THE_CLASSPATH=${THE_CLASSPATH}:${i}
 done
-echo ${THE_CLASSPATH}
 # compile all the scala class files in the src directory,
 # putting my jar file dependencies on the classpath, and writing
 # the ".class" files as output to the current directory:
 scalac -classpath "$THE_CLASSPATH" ./src/*.scala
 echo "Done compiling"
+
+# run the Runner class with the first and second bash parameters and the same classpath
 scala -classpath "$THE_CLASSPATH" Runner $1 $2
